dedde99da358171e5ef7f07c52848e81a425eb1e
tarangsachaniy <2021002171210143@ljku.edu.in>
2024-11-27 16:23:57 +0530

    "add analytics"

---
+++ src/app/api/analytics/route.js
@@ -0,0 +1,17 @@
+// app/api/analytics/route.js
+
+import { connectToDatabase } from "@/lib/mongodb";
+import Url from "@/models/Url";
+
+export async function GET() {
+  // Connect to the database
+  await connectToDatabase();
+
+  try {
+    const analyticsData = await Url.find({}, { text: 1 ,url : 1, slug: 1, count: 1 }); 
+    return new Response(JSON.stringify(analyticsData), { status: 200 });
+  } catch (error) {
+    console.error("Error fetching analytics data:", error);
+    return new Response("Failed to fetch analytics data", { status: 500 });
+  }
+}
